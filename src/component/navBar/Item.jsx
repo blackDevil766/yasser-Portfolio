f4affd441317e1fda74c3730a9f833333fd1b633
abdullahMohamed13 <https://github.com/abdullahMohamed13>
2025-07-16 08:55:24 +0300

    refactor: prop style added

--- src/component/navBar/Item.jsx
+++ src/component/navBar/Item.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Item(props) {
 
     return (
-        <li className="nav-item">
-            <a id="othersNavItemScroll" className="nav-link" href={props.linkIt}>{props.text}</a>
+        <li className="nav-item" >
+            <a id="othersNavItemScroll" style={props.style} className="nav-link" href={props.linkIt}>{props.text}</a>
         </li>
     )
 }
